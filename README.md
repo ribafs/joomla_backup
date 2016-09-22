a2994783843b74f9688722015b0e05a9e62563fa
Ribamar FS <contato@ribafs.org>
2016-09-22 19:03:01 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Simple Backup to CMS Joomla 2.5 and 3.x
+======================================
+
+[![Licença](https://img.shields.io/aur/license/yaourt.svg)](https://github.com/ribafs/simplebackup/blob/master/LICENSE)
+
+## Main Resources
+    Backup on server (folder backup)
+    Backup to download to desktop
+    Bypass 3 directories
+    Very simple backup (only 3 clicks)
+    Support Joomla 2.5 and Joomla 3.x
+    Translate to portuguese and english
+
+
+# Documentação/Documentation
+
+**Em português** and **In English**
+
+http://ribafs.org/portal/joomla/extensoes/simplebackup
+
+
+License
+-------
+
+The GPL 3.0 License (GPL 3)
---
+++ simplebackup.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ * @author-name Ribamar FS
+ * @copyright	Copyright (C) 2010 Ribamar FS.
+ * @license		GNU/GPL, see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * simplebackupfiles is free and open source software. This version may have been modified 
+ * pursuant to the GNU General Public License, and as distributed it includes or is 
+ * derivative of works licensed under the GNU General Public License or other free or 
+ * open source software licenses. 
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.filesystem.archive');
+jimport('joomla.filesystem.file');
+jimport('joomla.filesystem.folder');
+
+// Backup do banco
+$config = JFactory::getApplication(); 
+
+$dbhost = $config->getCfg('host');
+$dbuser = $config->getCfg('user');
+$dbpass = $config->getCfg('password');
+$database = $config->getCfg('db');
+
+if(!defined('DS')){
+	define('DS',DIRECTORY_SEPARATOR);
+}
+
+$date = date("d-m-Y_H-m");
+if(!file_exists(JPATH_SITE.DS.'backup')){
+	$back = mkdir(JPATH_SITE.DS.'backup', 0755);
+}
+
+$db_file = JURI::root().'backup'.DS.$database.'_'.$date.'.sql';
+
+// backup all tables in db
+function backup_tables($dbhost,$dbuser,$dbpass,$database,$db_file)
+{
+		$date = date("d-m-Y_H-m");
+
+        //connect to db
+        $link = mysqli_connect($dbhost,$dbuser,$dbpass);
+        mysqli_set_charset($link,'utf8');
+        mysqli_select_db($link,$database);
+
+        //get all of the tables
+        $tables = array();
+        $result = mysqli_query($link, 'SHOW TABLES');
+        while($row = mysqli_fetch_row($result))
+        {
+            $tables[] = $row[0];
+        }
+
+        //disable foreign keys (to avoid errors)
+        $return = 'SET FOREIGN_KEY_CHECKS=0;' . "\r\n";
+        $return.= 'SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";' . "\r\n";
+        $return.= 'SET AUTOCOMMIT=0;' . "\r\n";
+        $return.= 'START TRANSACTION;' . "\r\n";
+
+        //cycle through
+        foreach($tables as $table)
+        {
+            $result = mysqli_query($link, 'SELECT * FROM '.$table);
+            $num_fields = mysqli_num_fields($result);
+            $num_rows = mysqli_num_rows($result);
+            $i_row = 0;
+
+            //$return.= 'DROP TABLE '.$table.';'; 
+            $row2 = mysqli_fetch_row(mysqli_query($link,'SHOW CREATE TABLE '.$table));
+            $return.= "\n\n".$row2[1].";\n\n"; 
+
+            if ($num_rows !== 0) {
+                $row3 = mysqli_fetch_fields($result);
+                $return.= 'INSERT INTO '.$table.'( ';
+                foreach ($row3 as $th) 
+                { 
+                    $return.= '`'.$th->name.'`, '; 
+                }
+                $return = substr($return, 0, -2);
+                $return.= ' ) VALUES';
+
+                for ($i = 0; $i < $num_fields; $i++) 
+                {
+                    while($row = mysqli_fetch_row($result))
+                    {
+                        $return.="\n(";
+                        for($j=0; $j<$num_fields; $j++) 
+                        {
+                            $row[$j] = addslashes($row[$j]);
+                            $row[$j] = preg_replace("#\n#","\\n",$row[$j]);
+                            if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+                            if ($j<($num_fields-1)) { $return.= ','; }
+                        }
+                        if (++$i_row == $num_rows) {
+                            $return.= ");"; // last row
+                        } else {
+                            $return.= "),"; // not last row
+                        }   
+                    }
+                }
+            }
+            $return.="\n\n\n";
+        }
+
+        // enable foreign keys
+        $return .= 'SET FOREIGN_KEY_CHECKS=1;' . "\r\n";
+        $return.= 'COMMIT;';
+
+        //save file
+        $file='..'.DS.'backup'.DS.$database.'_'.$date.'.sql';
+
+        $handle = fopen($file,'w+');
+        fwrite($handle,$return);
+        fclose($handle);
+
+}
+
+// Créditos: http://stackoverflow.com/questions/81934/easy-way-to-export-a-sql-table-without-access-to-the-server-or-phpmyadmin
+
+// \Backup dpo Banco
+
+// Change php.ini directives
+ini_set('display_errors', 0);
+ini_set('memory_limit', '512M');
+ini_set('max_execution_time', 480); // Pode ser '120'
+?>
+
+<h1><?php print JText::_('COM_SIMPLEBACKUP_FILES_DATABASE');?></h1>
+<form action="" method="post" name="adminForm" id="adminForm">
+	<strong><?php print JText::_('COM_SIMPLEBACKUP_BYPASS');?></strong><br><br>
+	<input type="text" name="bypass1" value="backup"><br>
+	<input type="text" name="bypass2" value="download"><br>
+	<input type="text" name="bypass3" value=""><br>
+	<input type="submit" name="send" value="<?php print JText::_('COM_SIMPLEBACKUP_SEND');?>">
+</form>
+
+<?php
+
+// Scan directory and delete all files with extension
+// Example: remove_ext($file='/home/ribafs/teste.zip', $ext='zip')
+function remove_ext($file, $ext){
+	$handle = opendir($file);
+
+	//$name = JFile::stripExt($portal2);
+	// This is the correct way to loop over the directory.
+	while (false !== ($file2 = readdir($handle))) {
+		$ext2 =  JFile::getExt($file2);
+
+		if($ext2 == $ext && $file2 != '.' && $file2 != '..') {
+			unlink($file.DS.$file2);
+		}
+	}
+	closedir($handle);
+}
+
+remove_ext(JPATH_ROOT.DS.'tmp', 'sql');
+remove_ext(JPATH_ROOT.DS.'tmp', 'zip');
+
+// Pre-Load configuration
+require_once( JPATH_CONFIGURATION.DS.'configuration.php' );
+
+$config = JFactory::getApplication(); 
+$portal2 = '..'.DS.'backup'.DS.$database. '_'. $date . '.zip';
+
+if(JRequest::getVar('send')){
+
+$bypass1 = JPATH_SITE.DS.JRequest::getVar('bypass1');
+$bypass2 = JPATH_SITE.DS.JRequest::getVar('bypass2');
+$bypass3 = JPATH_SITE.DS.JRequest::getVar('bypass3');
+
+function Zip($source, $destination, $bypass1, $bypass2, $bypass3)
+{
+    if (!extension_loaded('zip') || !file_exists($source)) {
+        return false;
+    }
+
+    $zip = new ZipArchive();
+    if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+        return false;
+    }
+
+    $source = str_replace('\\', '/', realpath($source));
+
+    if (is_dir($source) === true)
+    {
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+        foreach ($files as $file){
+            $file = str_replace('\\', '/', realpath($file));
+
+            if (is_dir($file) === true){
+				if(($file == $bypass1) || ($file == $bypass2) || ($file == $bypass3)){
+					return false;
+				}else{
+	                $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+				}
+            }else if (is_file($file) === true){
+               	$zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+            }
+        }
+    }
+    else if (is_file($source) === true)
+    {
+        $zip->addFromString(basename($source), file_get_contents($source));
+    }
+
+    return $zip->close();
+	// Crédito: http://stackoverflow.com/questions/1334613/how-to-recursively-zip-a-directory-in-php
+}
+
+Zip("..".DS, $portal2, $bypass1, $bypass2, $bypass3);
+backup_tables($dbhost,$dbuser,$dbpass,$database,$db_file);
+	
+JFactory::getApplication()->enqueueMessage( JText::_('COM_SIMPLEBACKUP_SUCCESS'),'message');
+$backup = JURI::root().'backup';
+?>
+<h3>Downloads</h3>
+<a href="<?php print $portal2;?>">- <?php print JText::_('COM_SIMPLEBACKUP_FILES');?></a><br>
+<a href="<?php print $db_file;?>">- <?php print JText::_('COM_SIMPLEBACKUP_DATABASE');?></a>
+<?php
+}
+?>
